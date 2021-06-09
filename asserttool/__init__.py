e6cf1353ac0a72c2330f5a7612908adf6ba33ea1
jakeogh <github.com@v6y.net>
2021-06-09 12:13:16 -0700

    auto-commit

--- asserttool/__init__.py
+++ asserttool/__init__.py
@@ -1,5 +1,7 @@
 #from .asserttool import asserttool
 from .asserttool import maxone
 from .asserttool import minone
+from .asserttool import nevd
+from .asserttool import nl_iff_tty
 from .asserttool import one
 from .asserttool import verify

30ba895573347c2d4f2642ec70f3f91d6cfb74b4
jakeogh <github.com@v6y.net>
2023-05-10 12:39:58 -0700

    auto-commit

--- asserttool/__init__.py
+++ asserttool/__init__.py
@@ -1,5 +1,7 @@
 from .asserttool import am_root
 from .asserttool import click_validate_slice
+from .asserttool import gv
+from .asserttool import gvd
 from .asserttool import ic
 from .asserttool import icp
 from .asserttool import icr

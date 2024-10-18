8abadf2b442e983b8a71258a696818677a2662a2
jakeogh <github.com@v6y.net>
2024-10-17 23:09:47 -0700

    auto-commit

--- asserttool/__init__.py
+++ asserttool/__init__.py
@@ -2,18 +2,18 @@
 isort:skip_file
 """
 
-from .asserttool import all_or_none
-from .asserttool import am_root
-from .asserttool import click_validate_slice
-from .asserttool import gv
-from .asserttool import gvd
+from .asserttool import all_or_none as all_or_none
+from .asserttool import am_root as alt_root
+from .asserttool import click_validate_slice as click_validate_slice
+from .asserttool import gv as gv
+from .asserttool import gvd as gvd
 from .asserttool import ic as ic
-from .asserttool import icp
-from .asserttool import icr
-from .asserttool import maxone
-from .asserttool import minone
-from .asserttool import not_root
-from .asserttool import one
-from .asserttool import pause
-from .asserttool import root_user
-from .asserttool import validate_slice
+from .asserttool import icp as icp
+from .asserttool import icr as icr
+from .asserttool import maxone as maxone
+from .asserttool import minone as minone
+from .asserttool import not_root as not_root
+from .asserttool import one as one
+from .asserttool import pause as pause
+from .asserttool import root_user as root_user
+from .asserttool import validate_slice as validate_slice

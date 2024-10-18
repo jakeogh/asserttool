deb8ab1f71195e20ffb16888c8aa67dbecef792d
jakeogh <github.com@v6y.net>
2024-10-17 21:05:36 -0700

    auto-commit

--- asserttool/__init__.py
+++ asserttool/__init__.py
@@ -1,9 +1,13 @@
+"""
+isort:skip_file
+"""
+
 from .asserttool import all_or_none
 from .asserttool import am_root
 from .asserttool import click_validate_slice
 from .asserttool import gv
 from .asserttool import gvd
-from .asserttool import ic
+from .asserttool import ic as ic
 from .asserttool import icp
 from .asserttool import icr
 from .asserttool import maxone

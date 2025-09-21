69cdd5b498622dcd782c88758e693d99d5fbf1a3
jakeogh <github.com@v6y.net>
2025-09-20 20:46:36 -0700

    auto-commit

--- asserttool/__init__.py
+++ asserttool/__init__.py
@@ -4,7 +4,6 @@ isort:skip_file
 
 from .asserttool import all_or_none as all_or_none
 from .asserttool import am_root as am_root
-from .asserttool import click_validate_slice as click_validate_slice
 from .asserttool import gvd as gvd
 from .asserttool import ic as ic
 from .asserttool import icp as icp
@@ -15,4 +14,3 @@ from .asserttool import not_root as not_root
 from .asserttool import one as one
 from .asserttool import pause as pause
 from .asserttool import root_user as root_user
-from .asserttool import validate_slice as validate_slice
